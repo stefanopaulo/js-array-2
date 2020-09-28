7efa31eae9cdb47a809fe09923dfcd28552cb1d7
Stefano Paulo <stefanopaulo47@gmail.com>
2020-09-28 20:37:35 -0300

    map

---
+++ map.js
@@ -0,0 +1,32 @@
+// map -> retorna um novo array do mesmo tamanho do array original os valores alterados
+
+// Dobre os números
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const dobro = numeros.map(valor => valor * 2);
+console.log(dobro);
+
+// Para cada elemento:
+// Retorne apenas uma string com o nome da pessoa
+// Remova apenas a chave "nome" do objeto
+// Adicione uma chave id em cada objeto
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduarda', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+];
+
+const nomes = pessoas.map(obj => obj.nome);
+console.log(nomes);
+
+const idades = pessoas.map(obj => ({ idade: obj.idade })
+);
+console.log(idades);
+
+const comIds = pessoas.map((obj, indice) => {
+    obj.id =  (indice + 1) * 1000;
+    return obj;
+});
+console.log(comIds);
